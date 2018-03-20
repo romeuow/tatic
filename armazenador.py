91516859a9684281acb5f9f10f04892020e4df4d
Romeu <romeu.oliveira@powerlogic.com.r>
2018-03-19 23:32:37 -0300

    Finalizado

--- armazenador.py
+++ armazenador.py
@@ -1,14 +1,21 @@
 import sys
+import constantes
 
-inputTxt = open("input.txt",'r')
-outputTxt = open("arquivo_processado.txt",'w')
+#Abertura dos arquivos de entrada e saida
+inputTxt = open(sys.argv[1],'r')
+outputTxt = open(constantes.ARQUIVO_PROCESSADO,'w')
 
+#Carregando o conteudo
 with inputTxt as inp:
     conteudo = inp.readlines()
 
+#Ordenando os registros
 conteudo.sort()
+
+#Escrevendo registros ordenados no arquivo de saida
 with outputTxt as out:
 	out.writelines(conteudo)
 
+#Fechando arquivos
 inputTxt.close()
 outputTxt.close()
--- buscador.py
+++ buscador.py
@@ -1,45 +1,50 @@
 import sys
 import bisect
+import constantes
 
-def find_le(a, x):
-    'Find rightmost value less than or equal to x'
-    i = bisect.bisect_right(a, x)
-    if i:
-        return i-1
-    raise ValueError
+def encontra_maior(a, x):
+ 	#Encontra o maior registro menor ou igual a x
+    j = bisect.bisect_right(a, x)
+    return j
 
-def find_ge(a, x):
-    'Find leftmost item greater than or equal to x'
+def encontra_menor(a, x):
+    #Encontra o menor registro maior ou igual a x
     i = bisect.bisect_left(a, x)
-    if i != len(a):
-        return i
-    raise ValueError
+    return i
 
+#Validacao de dados obrigatorios
 if (len(sys.argv) < 3):
-	print "Forneca data inicial e data final para o algoritmo de busca"
+	print "Forneca data inicial e data final para o algoritmo de busca."
 	exit(1)
 
-inputTxt = open("arquivo_processado.txt",'r')
-outputTxt = open("saida.txt",'w')
+#Abertura dos arquivos de entrada e saida
+inputTxt = open(constantes.ARQUIVO_PROCESSADO,'r')
+outputTxt = open(constantes.RESULTADO,'w')
 
+#Iniciando as listas
 registro = []
 data = []
-tag = []
 
+#Armazena registros ordenados e os formata
 for line in inputTxt:
 	linha = tuple(line.strip('\n').split(';'))
-	#print linha
 	registro.append(linha)
 	data.append(linha[0])
-	tag.append(linha[1])
-	
-#print find_le(conteudo,'20170113153016356')
 
-print find_ge(data, sys.argv[1])
-print find_le(data, sys.argv[2])
+#Realiza busca por data e retor os indices dos registros
+i = encontra_menor(data, sys.argv[1])
+j = encontra_maior(data, sys.argv[2])
 
+#Busca apenas por intervalos de datas
+if len(sys.argv) == 3:
+	outputTxt.write('\n'.join('{} {} {}'.format(x[0],x[1],x[2]) for x in registro[i:j]))
 
-print len(sys.argv)
+#Filtra por identificador do evento
+else:
+	condition = list(sys.argv[3:])
+	filtro = [x for x in registro[i:j] if x[1] in condition]
+	outputTxt.write('\n'.join('{} {} {}'.format(x[0],x[1],x[2]).replace(' ',';') for x in filtro))
 
+#Fecha arquivos de entrada e saida
 inputTxt.close()
 outputTxt.close()
